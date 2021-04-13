6ae4873096a62db93b53494598a5d7a52135c8f6
Ryu, Gyoung-Seok <narzis@gmail.com>
2021-04-13 15:31:55 +0900

    save

--- prepare.bash
+++ prepare.bash
@@ -19,7 +19,7 @@ useradd -g dba tibero
 #output "--- Downloading Tibero Database Software via WGET ---"
 #wget -q --load-cookies /tmp/cookies.txt \
 #"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PdRlSnuH2-e3THVQ2G7_NtiWHrN3B46w' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PdRlSnuH2-e3THVQ2G7_NtiWHrN3B46w" \
-#-O /tmp/tibero6-bin-FS07_CS_1912-linux64-174424-opt.tar.gz && rm -rf /tmp/cookies.txt
+#-O /tmp/tibero5sp1-bin-FS07_CS_1912-linux64-174424-opt.tar.gz && rm -rf /tmp/cookies.txt
 
 output "--- Untar Tibero Software ---"
 su - tibero -c "tar -xf /tmp/tibero5-bin-FS05_CS_1902-linux64-166170-opt.tar.gz"
@@ -34,8 +34,8 @@ output "--- Downloading necessary files to run ---"
 #Until End of April 2021
 wget --load-cookies /tmp/cookies.txt \
 "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mRUj19dZmrqx6lW91QBZn1H7Jn4gglp4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mRUj19dZmrqx6lW91QBZn1H7Jn4gglp4" \
--O /home/tibero/tibero6/license/license.xml && rm -rf /tmp/cookies.txt
-chown tibero:dba /home/tibero/tibero6/license/license.xml 
+-O /home/tibero/tibero5sp1/license/license.xml && rm -rf /tmp/cookies.txt
+chown tibero:dba /home/tibero/tibero5sp1/license/license.xml 
 
 output "--- ENTRYPOINT start.bash to tibero HOME  ---"
 su - tibero -c "wget -q https://raw.githubusercontent.com/dimensigon/tibero-docker/master/start.bash \
